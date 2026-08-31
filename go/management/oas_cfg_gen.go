e8e7148e21f8fdba790c89331e8a2e144f86be4b
Roshan Jonnalagadda <roshan@jonahnz.com>
2026-09-01 09:25:53 +1000

    fix: sync mailbox reliability clients

--- go/management/oas_cfg_gen.go
+++ go/management/oas_cfg_gen.go
@@ -12,9 +12,13 @@ import (
 	ht "github.com/ogen-go/ogen/http"
 	"github.com/ogen-go/ogen/middleware"
 	"github.com/ogen-go/ogen/ogenerrors"
+	"github.com/ogen-go/ogen/ogenregex"
 	"github.com/ogen-go/ogen/otelogen"
 )
 
+var regexMap = map[string]ogenregex.Regexp{
+	"^(?![^@]*\\.\\.)[a-zA-Z0-9_%+-](?:[a-zA-Z0-9._%+-]*[a-zA-Z0-9_%+-])?@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,63}$": ogenregex.MustCompile("^(?![^@]*\\.\\.)[a-zA-Z0-9_%+-](?:[a-zA-Z0-9._%+-]*[a-zA-Z0-9_%+-])?@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,63}$"),
+}
 var (
 	// Allocate option closure once.
 	clientSpanKind = trace.WithSpanKind(trace.SpanKindClient)
--- packages/php/management/src/Model/ManagementCreateMailboxRequest.php
+++ packages/php/management/src/Model/ManagementCreateMailboxRequest.php
@@ -292,6 +292,10 @@ class ManagementCreateMailboxRequest implements ModelInterface, ArrayAccess, Jso
             $invalidProperties[] = "invalid value for 'email', the character length must be bigger than or equal to 5.";
         }
 
+        if (!preg_match("/^(?![^@]*\\.\\.)[a-zA-Z0-9_%+-](?:[a-zA-Z0-9._%+-]*[a-zA-Z0-9_%+-])?@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,63}$/", $this->container['email'])) {
+            $invalidProperties[] = "invalid value for 'email', must be conform to the pattern /^(?![^@]*\\.\\.)[a-zA-Z0-9_%+-](?:[a-zA-Z0-9._%+-]*[a-zA-Z0-9_%+-])?@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,63}$/.";
+        }
+
         if (!is_null($this->container['quota_bytes']) && ($this->container['quota_bytes'] < 1)) {
             $invalidProperties[] = "invalid value for 'quota_bytes', must be bigger than or equal to 1.";
         }
@@ -369,6 +373,9 @@ class ManagementCreateMailboxRequest implements ModelInterface, ArrayAccess, Jso
         }
         if ((mb_strlen($email) < 5)) {
             throw new InvalidArgumentException('invalid length for $email when calling ManagementCreateMailboxRequest., must be bigger than or equal to 5.');
+        }
+        if ((!preg_match("/^(?![^@]*\\.\\.)[a-zA-Z0-9_%+-](?:[a-zA-Z0-9._%+-]*[a-zA-Z0-9_%+-])?@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,63}$/", ObjectSerializer::toString($email)))) {
+            throw new InvalidArgumentException("invalid value for \$email when calling ManagementCreateMailboxRequest., must conform to the pattern /^(?![^@]*\\.\\.)[a-zA-Z0-9_%+-](?:[a-zA-Z0-9._%+-]*[a-zA-Z0-9_%+-])?@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,63}$/.");
         }
 
         $this->container['email'] = $email;
